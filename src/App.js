6ec85a66d23e22146d706e85e9f900a5e799c39c
rohit2004metha <rohitmetha111@gmail.com>
2024-08-07 16:04:40 +0530

    First Commit by Rohit Metha

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Suspense, lazy } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import StartCanvas from "./components/canvas/Stars";
+
+// Lazy load sections
+const Hero = lazy(() => import("./components/sections/Hero"));
+const Skills = lazy(() => import("./components/sections/Skills"));
+const Experience = lazy(() => import("./components/sections/Experience"));
+const Education = lazy(() => import("./components/sections/Education"));
+const Projects = lazy(() => import("./components/sections/Projects"));
+const Contact = lazy(() => import("./components/sections/Contact"));
+const Footer = lazy(() => import("./components/sections/Footer"));
+
+// Styled components
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <StartCanvas />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Hero />
+            <Wrapper>
+              <Skills />
+              <Experience />
+            </Wrapper>
+            <Projects />
+            <Wrapper>
+              <Education />
+              <Contact />
+            </Wrapper>
+            <Footer />
+          </Suspense>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,412 @@
+import actualone from '../images/Actualone.png';
+import Zion from '../images/Zion.png';
+import Tedx from '../images/tedx.png';
+import Zo from '../images/Zo.png';
+import Devkraft from '../images/devkrate.jpeg';
+import YouTube from '../images/YouTube.png';
+import Instagram from '../images/instagram.png';
+import DPU from '../images/DPU.png';
+import Ae from '../images/Ae.png';
+import pr from '../images/Pr.png';
+import Dr from '../images/DaVinci Resolve.png';
+import canva from '../images/Canva.png';
+import Lr from '../images/Lightroom.png';
+import Ai from '../images/Ai.png';
+import Figma from '../images/Figma.png';
+import CC from '../images/CapCut.png';
+import Pi from '../images/Pixelab.png';
+import Collage from '../images/collage.png';
+import Shool from '../images/Shool.png';
+import Am from '../images/Am.png';
+
+export const Bio = {
+  name: "Kunal Bhoi",
+  roles: [
+    "video editor",
+    "photographer",
+    "videographer",
+  ],
+  description: 
+    "I’m Kunal. With my varied experience in content creation, I bring a unique blend of skills to the table in Video Editing, leading media teams, and innovating in Web3 technology. Notably, I've grown an Instagram page to 150k followers and managed a popular YouTube channel with 30 million views. With my mix of creativity, strategy, and hands-on experience, I'm ready to make a big impact.",
+  socialLinks: {
+    instagram: "https://instagram.com/kunaaal_.8401",
+    resume: "https://drive.google.com/file/d/1cK5wK0WPihE17kty2vtjuGr4UlQ6vqQy/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/kunal-bhoi-b961a3232/",
+    twitter: "https://x.com/kunalbhoi_/",
+    facebook: "https://www.facebook.com/profile.php?id=100009185008934",
+    telegram: "https://t.me/Kunaaal_8401",
+  }
+};
+
+export const skills = [
+  {
+    title: "Computer",
+    skills: [
+      { name: "After Effect", image: Ae },
+      { name: "Premiere Pro", image: pr },
+      { name: "Davinci Resolve", image: Dr },
+      { name: "Canva", image: canva },
+      { name: "Lightroom", image: Lr },
+      { name: "Adobe Illustrator", image: Ai },
+      { name: "Figma", image: Figma },
+    ],
+  },
+  {
+    title: "Mobile",
+    skills: [
+      { name: "CapCut", image: CC },
+      { name: "Pixellab", image: Pi },
+      { name: "Alight Motion", image: Am },
+      { name: "Canva", image: canva },
+      { name: "Lightroom", image: Lr },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: Instagram,
+    role: "Influencer",
+    company: "Instagram",
+    date: "May 2018 - Present",
+    desc: "I've created a page on Instagram (High Clicks Status) with 160k+ followers. Grew this page from 0 to 160k+ in the last 3 years. I do promotions and collaborations on that channel and have managed profiles of many users.",
+    skills: [
+      "Video Editing",
+      "Social Media Marketing",
+      "Social Media Managing",
+    ],
+  },
+  {
+    id: 1,
+    img: YouTube,
+    role: "Video Editor",
+    company: "YouTube",
+    date: "Sep 2022 - Present",
+    desc: "I'm currently running a YouTube channel that has 5.5K+ subscribers and has reached up to 30M views.",
+  },
+  {
+    id: 2,
+    img: Tedx,
+    role: "Production Lead",
+    company: "TEDxDYPIT",
+    date: "Sep 2022 - Sep 2023",
+    desc: "As a production lead at TEDx, I managed a team and coordinated effectively. Together, we successfully handled campaigning and marketing. I ensure that every piece of content not only aligns with our strategic goals but also resonates deeply with our audience.",
+    skills: [
+      "Video Editing",
+      "Campaigning",
+      "Marketing",
+    ],
+  },
+  {
+    id: 3,
+    img: actualone,
+    role: "Video Editor",
+    company: "Actualone",
+    date: "Jun 2022 - Present",
+    desc: "I began working at Actualone in my first year, where I played a key role in expanding the company by meeting market demand. My responsibilities included capturing and editing content to produce the final output. Additionally, we organized events in colleges to shoot videos on a range of topics, enhancing our content offerings and engagement.",
+    skills: [
+      "Creative Storytelling",
+      "Time Efficiency",
+      "Effective Communication",
+    ],
+  },
+  {
+    id: 4,
+    img: Devkraft,
+    role: "Head of Production",
+    company: "DevKraft",
+    date: "Feb 2022 - Present",
+    desc: "In my first year, I co-founded a club called Devkraft with a few others, where I served as the social media manager. I managed the club’s online profile, which helped me develop my leadership skills. My experience there led to my promotion to production management lead. We collaborated with other clubs to shoot their events, and my primary role was to shoot and edit these events for social media. I also managed an official team of 8 to 10 members responsible for photography and editing.",
+    skills: [
+      "Leadership Excellence",
+      "Strategic Planning",
+      "Creative Vision",
+      "Problem-Solving Prowess",
+    ],
+  },
+  {
+    id: 5,
+    img: Devkraft,
+    role: "Social Media Manager",
+    company: "DevKraft",
+    date: "Mar 2022 - Dec 2022",
+    desc: "At DevKraft, I infuse creativity and strategy into our social media presence, crafting a vibrant digital persona that resonates with our audience. As the Social Media Manager, I orchestrate innovative campaigns that tell compelling stories and drive meaningful engagement, ensuring our brand stands out in the crowded digital landscape.",
+    skills: [
+      "Creative Vision",
+      "Analytical Acumen",
+      "Communication",
+      "Project Management",
+      "Online Campaigning",
+    ],
+  },
+  {
+    id: 6,
+    img: Zion,
+    role: "Production Lead",
+    company: "Zion",
+    date: "Feb 2023 - Present",
+    desc: "Zion is a club where all the clubs from the college collaborate to conduct one big event. Through my work status and overall experience, I successfully attained the position of production head in my second year. My responsibilities included shooting reels for campaigning, marketing, and promotions, and editing these reels for social media platforms. Here, I learned valuable skills in time management and leadership.",
+    skills: [
+      "Leadership",
+      "Time Management",
+      "Videography",
+    ],
+  },
+  {
+    id: 7,
+    img: DPU,
+    role: "Official Photographer",
+    company: "DYPIT",
+    date: "Feb 2022 - Present",
+  },
+  {
+    id: 8,
+    img: Zo,
+    role: "Video Editor",
+    company: "Zo Lab",
+    date: "Mar 2024 - Aug 2024",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: DPU,
+    school: "Dr. D. Y. Patil Institute of Technology Pimpri-Chinchwad, Maharashtra",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.41 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in ENTC and Engineering at Dr.D.Y.Patil Institute of Technology Pimpri-Chinchwad, Maharashtra.",
+    degree: "Bachelor of Technology - BTech, ENTC and Engineering",
+  },
+  {
+    id: 1,
+    img: Collage,
+    school: "Shri Palasnath Vidyalay Indapur, Maharashtra",
+    date: "Apr 2019 - Apr 2021",
+    grade: "88.2%",
+    desc: "I completed my class 12 high school education at Shri Palasnath Vidyalay Indapur, Maharashtra, where I studied Science.",
+    degree: "HSC(XII), Science",
+  },
+  {
+    id: 2,
+    img: Shool,
+    school: "Shri Narayandas Ramdas HighSchool Indapur, Maharashtra",
+    date: "Apr 2007 - Apr 2019",
+    grade: "82.82%",
+    desc: "I completed my class 10 education at Narayandas Ramdas HighSchool Indapur, Maharashtra, where I studied Science.",
+    degree: "SSC(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    videourl: "https://drive.google.com/file/d/1lALKwq5uoh14cijNl3gJ5y5LrxVGhLQI/view?usp=drive_link",
+    img: Instagram,
+    title: "Influencer",
+    description: 
+      "On Instagram, I have created a page (High Clicks Status) with 160k+ followers, growing it from 0 to 160k+ in the last 3 years. I do promotions and collaborations on that channel and have managed profiles of many users.",
+  },
+  {
+    id: 2,
+    videourl: "https://drive.google.com/file/d/1jlk7GFErqT5vPl81wVbFrX4Jd1PqGOXe/view?usp=drive_link",
+    img: YouTube,
+    title: "YouTube",
+    description: 
+      "I currently run a YouTube channel with 5.5K+ subscribers that has reached up to 30M views.",
+  },
+  {
+    id: 3,
+   videourl:
+      "https://drive.google.com/file/d/1oZLLysNvut2bSj-zJmZvhJvu9HwtdV9d/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/1oZLLysNvut2bSj-zJmZvhJvu9HwtdV9d/view?usp=drivesdk",
+  },
+  {
+    id: 4,
+    videourl :
+      "https://drive.google.com/file/d/1jAEEGr4ax_a-EspTD6h2FMcnVS8pWBVu/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/1jAEEGr4ax_a-EspTD6h2FMcnVS8pWBVu/view?usp=drivesdk",
+  },
+  {
+    id: 5,
+    videourl :
+      "https://drive.google.com/file/d/1olkjpgm1RlQVscwn9bI46Mnbd5R0nbB3/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/1olkjpgm1RlQVscwn9bI46Mnbd5R0nbB3/view?usp=drivesdk",
+  },
+  {
+    id: 6,
+    videourl :
+      "https://drive.google.com/file/d/1opiCcc2oCVLFFtIM7ux94FllhpAiTD_i/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/1opiCcc2oCVLFFtIM7ux94FllhpAiTD_i/view?usp=drivesdk",
+  },
+  {
+    id: 7,
+    videourl :
+      "https://drive.google.com/file/d/16rck6wUApijiVLPVLLeMahmBJKRcri7m/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/16rck6wUApijiVLPVLLeMahmBJKRcri7m/view?usp=drivesdk",
+  },
+  {
+    id: 8,
+    videourl :
+      "https://drive.google.com/file/d/1EIPvsdl_RboVJ3rBAmLUhotGtouC4ZPU/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/1EIPvsdl_RboVJ3rBAmLUhotGtouC4ZPU/view?usp=drivesdk",
+  },
+  {
+    id: 9,
+    videourl :
+      "https://drive.google.com/file/d/1XFCgnownthZi2DpcLcRLJDrwDrFXFGau/view?usp=drivesdk",
+    category: "Video Editing",
+    Fullvideolink: "https://drive.google.com/file/d/1XFCgnownthZi2DpcLcRLJDrwDrFXFGau/view?usp=drivesdk",
+  },
+  
+  {
+    id: 1,
+    videourl:
+      "https://drive.google.com/file/d/1L0h9fv25fwIw8pmptYV_dCPOIEPwOoUH/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1L0h9fv25fwIw8pmptYV_dCPOIEPwOoUH/view?usp=drive_link",
+  },
+  {
+    id: 2,
+    videourl:
+      "https://drive.google.com/file/d/11KZsHZDoIded2RBVnLPbtGbPXwuX1aaf/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/11KZsHZDoIded2RBVnLPbtGbPXwuX1aaf/view?usp=drive_link",
+  },
+  
+  {
+    id: 3,
+    videourl:
+      "https://drive.google.com/file/d/1eDYgNDh0Yq1EfB7BY-BJZhSQBaz0pH5G/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1eDYgNDh0Yq1EfB7BY-BJZhSQBaz0pH5G/view?usp=drive_link",
+  },
+  {
+    id: 4,
+    videourl:
+      "https://drive.google.com/file/d/1f7vK6NOX_BnQxzhXKv8K6hryHABLt8WM/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1f7vK6NOX_BnQxzhXKv8K6hryHABLt8WM/view?usp=drive_link",
+  },
+  {
+    id: 5,
+    videourl:
+      "https://drive.google.com/file/d/1sorca_413V5mNIJbCnSExf69BbKAHh-o/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1sorca_413V5mNIJbCnSExf69BbKAHh-o/view?usp=drive_link",
+  },
+  {
+    id: 6,
+    videourl:
+      "https://drive.google.com/file/d/1ktXv_gUOTY9eJGnHgCstBA8mIs2zjka0/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1ktXv_gUOTY9eJGnHgCstBA8mIs2zjka0/view?usp=drive_link",
+  },
+  {
+    id: 7,
+    videourl:
+      "https://drive.google.com/file/d/12jGGTQVAMe9ZvwyZJ-_Po9xkJp42X4ls/view?usp=sharing",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/12jGGTQVAMe9ZvwyZJ-_Po9xkJp42X4ls/view?usp=sharing",
+  },
+  {
+    id: 8,
+    videourl:
+      "https://drive.google.com/file/d/1HRfaqjp5sk-SSs5f9kiBslDzAsE7aSw3/view?usp=sharing",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1HRfaqjp5sk-SSs5f9kiBslDzAsE7aSw3/view?usp=sharing",
+  },
+  {
+    id: 9,
+    videourl:
+      "https://drive.google.com/file/d/1XorGdAeEIg0UUzEB7vJUSUEN_QqXfzqk/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1XorGdAeEIg0UUzEB7vJUSUEN_QqXfzqk/view?usp=drive_link",
+  },
+  {
+    id: 10,
+    videourl:
+      "https://drive.google.com/file/d/1mGgiNGZMcTmyJpCEJ49NXKQXHSZu89tc/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1mGgiNGZMcTmyJpCEJ49NXKQXHSZu89tc/view?usp=drive_link",
+  },
+  {
+    id: 11,
+    videourl:
+      "https://drive.google.com/file/d/14xtgDXG4d-UjGDuuxopojKBjC2okbvvU/view?usp=sharing",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/14xtgDXG4d-UjGDuuxopojKBjC2okbvvU/view?usp=sharing",
+  },
+  {
+    id: 12,
+    videourl:
+      "https://drive.google.com/file/d/1vfjrmDVHxU01lVFlg3P1XngZKW_yUShb/view?usp=drive_link",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1vfjrmDVHxU01lVFlg3P1XngZKW_yUShb/view?usp=drive_link",
+  },
+  {
+    id: 13,
+    videourl:
+      "https://drive.google.com/file/d/1ZG06XHltNSvLUfovNZ7oVjZC2oxu87zv/view?usp=sharing",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/1ZG06XHltNSvLUfovNZ7oVjZC2oxu87zv/view?usp=sharing",
+  },
+  {
+    id: 14,
+    videourl:
+      "https://drive.google.com/file/d/18_aPW1O0vLPBOZIYHRPrb69OIGHE0Xk2/view?usp=sharing",
+    category: "Photography",
+    Fullphotolink: "https://drive.google.com/file/d/18_aPW1O0vLPBOZIYHRPrb69OIGHE0Xk2/view?usp=sharing",
+  },
+  
+  {
+    id: 1,
+  videourl:
+      "https://drive.google.com/file/d/1sezu4hCKPQJqmQ6cK2bUiRcxSHTwSb2x/view?usp=drive_link",
+    category: "Videographer",
+    Fullvideolink: "https://drive.google.com/file/d/1sezu4hCKPQJqmQ6cK2bUiRcxSHTwSb2x/view?usp=drive_link",
+  },
+  {
+    id: 2,
+    videourl:
+      "https://drive.google.com/file/d/1rBcTSMXnAjQpWRVnvXo2jYSyRT0s22WU/view?usp=drive_link",
+    category: "Videographer",
+    Fullvideolink: "https://drive.google.com/file/d/1rBcTSMXnAjQpWRVnvXo2jYSyRT0s22WU/view?usp=drive_link",
+  },
+  {
+    id: 3,
+    videourl:
+      "https://drive.google.com/file/d/1trjaOFZu3awqcbsupT1zn3JMb1pgVOUc/view?usp=drive_link",
+    category: "Videographer",
+    Fullvideolink: "https://drive.google.com/file/d/1trjaOFZu3awqcbsupT1zn3JMb1pgVOUc/view?usp=drive_link",
+  },
+  {
+    id: 4,
+    videourl:
+      "https://drive.google.com/file/d/1SPWe0JEKT4wuYNM_kfP524NBp_N6OmlF/view?usp=drive_link",
+    category: "Videographer",
+    Fullvideolink: "https://drive.google.com/file/d/1SPWe0JEKT4wuYNM_kfP524NBp_N6OmlF/view?usp=drive_link",
+  },
+  {
+    id: 5,
+    videourl:
+      "https://drive.google.com/file/d/10oMtLBhbH5CqC8l1dhSALiid8NtovJhV/view?usp=drive_link",
+    category: "Videographer",
+    Fullvideolink: "https://drive.google.com/file/d/10oMtLBhbH5CqC8l1dhSALiid8NtovJhV/view?usp=drive_link",
+  },
+  {
+    id: 6,
+    videourl:
+      "https://drive.google.com/file/d/1GueEMSQZ3isrXoMqSVTQYb3LfZXOdZdn/view?usp=drive_link",
+    category: "Videographer",
+    Fullvideolink: "https://drive.google.com/file/d/1GueEMSQZ3isrXoMqSVTQYb3LfZXOdZdn/view?usp=drive_link",
+  },
+];
+
