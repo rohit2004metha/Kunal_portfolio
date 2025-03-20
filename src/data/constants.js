826bf9b5a425ccc0a5e694a61a0b2b4f8b12c0bc
ROHIT METHA <131907078+rohit2004metha@users.noreply.github.com>
2025-03-20 23:26:27 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -73,31 +73,6 @@ export const skills = [
       },
     ],
   },
-  {
-    title: "Mobile",
-    skills: [
-      {
-        name: "CapCut",
-        image: CC
-      },
-      {
-        name: "Pixellab",
-        image: Pi
-      },
-      {
-        name: "Alight Motion",
-        image: Am
-      },
-      {
-        name: "Canva",
-        image: canva
-      },
-      {
-        name: "Lightroom",
-        image: Lr
-      },
-    ],
-  },
 ]
  
 
@@ -105,14 +80,16 @@ export const experiences = [
   {
     id: 0,
     img: Instagram,
-    role:  "Influencer",
+    role:  "Instagram Influencing",
     company: " Instagram ",
     date: "May 2018 - Present",
-    desc: " I've created a page on Instagram {[High Clicks Status] [160k+ followers]}. Grew this page from 0 to 160k+ in last 3 years. And also I do promotions and collaborations on that channel. And I have Managed profile's of many users ",
+    desc: "Grew my page from 0 to 160k Followers",
+    desc: "30M+ Weekly Impressions",
     skills :[
+      "Content Strategy",
+      "Trend Identification",
       "Video Editing",
-      "Social Media Marketing ",
-      "Social Media Managing ",
+      "SMM",
     ]
   },
   {
@@ -121,92 +98,56 @@ export const experiences = [
     role: " Video Editor ",
     company: " YouTube ",
     date: "sep 2022 - Present",
-    desc: " I'm currently running a youtube channel that has (5.5K+ subscribers). And reached upto 30M views. ",
+    desc: " Build my channel from 0 to 5.5K followers. ",
+    desc: " Over 35M+ Views. ",
+    desc: " 400M+ Impressions. ",
+    skills :[
+      "Video Editing",
+      "Content Calendar Management",
+    ]
   },
   {
     id: 2,
-    img: Tedx,
-    role: " Production Lead ",
-    company: "TEDxDYPIT",
-    date: "Sep 2022 - Sep 2023",
-    desc: " As a production lead at TEDx, I managed a team of  people and coordinated it effectively. Together, we successfully handled campaigning and marketing.I ensure that every piece of content not only aligns with our strategic goals but also resonates deeply with our audience. ",
-    skills: [
-      "Video Editing",
-      "Campaigning",
-      "Marketing",
+    img : actualone,
+    role: " Production Head ",
+    company: " Actualone ",
+    date: "Jun 2022 - Present",
+    desc: "Grew Actualone's Reach on basis of content by 250%",
+      desc: "Reaching 1M+ audience.",
+      desc: "Grew company network in Goa and Bangalore WEB 3.",
+    skills :[
+      "Communication",
+      "Relation Building",
+      "Production",
     ],
   },
   {
     id: 3,
-    img : actualone,
-    role: " Video Editor ",
-    company: " Actualone ",
-    date: "Jun 2022 - Present",
-    desc: "I began working at Actualone in my first year, where I played a key role in expanding the company by meeting market demand. My responsibilities included capturing and editing content to produce the final output. Additionally, we organized events in colleges to shoot videos on a range of topics, enhancing our content offerings and engagement",
-    skills :[
-      "Creative Storytelling",
-      "Time Efficiency",
-      "Effective Communication",
-    ],
-  },
-  {
-    id: 4,
     img: Devkraft,
-    role: " Head of Production ",
+    role: " Co-founder and Production head ",
     company:  " DevKraft ",
     date: "Feb 2022 - Present",
-    desc: "In my first year, I co-founded a club called Devkraft with a few others, where I served as the social media manager. I managed the club’s online profile, which helped me develop my leadership skills. My experience there led to my promotion to production management lead. We collaborated with other clubs to shoot there events, and my primary role was to shoot and edit these events for social media. I also managed an official team of 8 to 10 members responsible for photography and editing.",
-    skills: [
-      "Leadership Excellence",
-      "Strategic Planning",
-      "Creative Vision",
-      "Problem-Solving Prowess",
-    ],
-  },
-  {
-    id: 5,
-    img: Devkraft,
-    role: " Socil Media Manager ",
-    company: "DevKraft",
-    date: "Mar 2022 - Dec 2022",
-    desc: " At DevKraft, I infuse creativity and strategy into our social media presence, crafting a vibrant digital persona that resonates with our audience. As the Social Media Manager, I orchestrate innovative campaigns that tell compelling stories and drive meaningful engagement, ensuring our brand stands out in the crowded digital landscape ",
-    skills: [
-      "Creative Vision",
-      "Analytical Acumen",
-      "Communication",
-      "Project Management",
-      "Online Campaigning ",
-    ],
-  },
-  {
-    id: 6,
-    img: Zion,
-    role: "Production Lead",
-    company: "Zion",
-    date: "Feb 2023 - Present",
-    desc: "Zion is a club where all the clubs from the college collaborate to conduct one big event. Through my work status and overall experience, I successfully attained the position of production head in my second year. My responsibilities included shooting reels for campaigning, marketing, and promotions, and editing these reels for social media platforms. Here, I learned valuable skills in time management and leadership. ",
+    desc: "Handled team of 100+ individuals in various teams.",
+    desc: "- Organisation production highlights reach 1M+.",
     skills: [
       "Leadership",
-      "Time Management ",
-      "Videogrphy",
-      
+      "Marketing",
+      "Production",
     ],
   },
-  {
-    id: 7,
-    img: DPU,
-    role: " Official Photographer ",
-    company: "DYPIT",
-    date: "Feb 2022 - Present",
-  },
-  {
-    id: 8,
-    img: Zo,
-    role: "Video Editor",
-    company: "Zo Lab",
-    date: "Mar 2024 - Aug 2024",
+ {
+    id: 4,
+    img: Tedx,
+    role: " Production Lead ",
+    company: "TEDxDYPIT",
+    date: "Sep 2022 - Sep 2023",
+    desc: "Managed a team of 12+ content creators and crew.",
+   desc: "Created and shot marketing campaign in 10+ colleges.",
+    skills: [
+      "Leadership",
+      "Content Calendar",
+      "Content Strategy",
+    ],
   },
 
-  
-  
 ];
